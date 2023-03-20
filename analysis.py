54f38b7703e211a36af0fccbd9b27097aafba15f
Gon Nespral <gonespral@me.com>
2023-03-20 15:29:43 +0100

    Additional features

--- analysis.py
+++ analysis.py
@@ -10,15 +10,13 @@ import numpy as np  # for FFT
 import matplotlib.pyplot as plt  # for plotting
 import pickle
 
-# Sample rate of the data
+
+# Configuration parameters
 sample_rate = 48128
-
-# Number of points to smooth FFT by
 n_smoothing = 10
-
-# x-axis limits for plots (frequency)
 x_lim_0 = 1
-x_lim_1 = 20000
+x_lim_1 = 500
+NFFT_val = 256
 
 # Path to pickle files
 pickle_paths = ["data/processed/8m_s.pkl",
@@ -33,24 +31,26 @@ def info():
     Print a list of commands that can be used in this script.
     :return:
     """
-    print("--- Available commands ---")
+    print("\n--- Available commands ---")
     # Get dir() of functions of  global namespace written by user
     for name, obj in globals().items():
         if callable(obj) and obj.__module__ == "__main__":
-            print(f"-> {name}()")
+            print(f"    {name}()")x<_lim
     print()
 
     print("--- Available variables ---")
-    print(f"-> sample_rate = {sample_rate}")
-    print(f"-> n_smoothing = {n_smoothing}")
-    print(f"-> x_lim_0 = {x_lim_0}")
-    print(f"-> x_lim_1 = {x_lim_1}")
+    print(f"    sample_rate = {sample_rate}")
+    print(f"    n_smoothing = {n_smoothing}")
+    print(f"    x_lim_0 = {x_lim_0}")
+    print(f"    x_lim_1 = {x_lim_1}")
+    print(f"    NFFT_val = {NFFT_val}")
     print()
 
     print("--- Loaded data ---")
     for i, measurement in enumerate(measurements):
-        print(f"-> measurements[{i}] | {measurement['v_inf']}m/s | {pickle_paths[i]}")
+        print(f"    measurements[{i}] : {measurement['v_inf']}m/s \n    {pickle_paths[i]}\n")
     print()
+
 
 def plot_denoised_fft_all():
     """
@@ -74,12 +74,13 @@ def plot_denoised_fft_all():
 
 def plot_denoised_fft_single(i: int):
     """
-    Display denoised FFT of a single measurement.
+    Display denoised FFT of a single measurement. Uses rolling mean to smooth the data.
     :param i: measurement index
     :return:
     """
     fig, ax = plt.subplots()
     df_wt_bg_fft = measurements[i]["df_wt_bg_fft"]
+    df_wt_bg_fft["fft"] = df_wt_bg_fft["fft"].rolling(n_smoothing).mean()
     ax.plot(df_wt_bg_fft["freq"],
             df_wt_bg_fft["fft"],
             label=f"{measurements[i]['v_inf']}m/s")
@@ -93,7 +94,7 @@ def plot_denoised_fft_single(i: int):
 
 def plot_denoised_psd_single(i: int):
     """
-    Plot Power Spectral Density of a single measurement, using plt.psd Uses rolling mean to smooth the data.
+    Plot Power Spectral Density of a single measurement, using plt.psd.
     :param i: measurement index
     :return:
     """
@@ -101,6 +102,7 @@ def plot_denoised_psd_single(i: int):
     df_wt_bg_fft = measurements[i]["df_wt_bg_fft"]
     ax.psd(df_wt_bg_fft["fft"],
            Fs=sample_rate,
+           NFFT=NFFT_val,
            label=f"{measurements[i]['v_inf']}m/s")
     ax.set_xscale("log")
     ax.set_xlim(x_lim_0, x_lim_1)
@@ -122,7 +124,7 @@ def plot_denoised_psd_all():
         df_wt_bg_fft = measurement["df_wt_bg_fft"]
         ax.psd(df_wt_bg_fft["fft"],
                Fs=sample_rate,
-               NFFT=1024,
+               NFFT=NFFT_val,
                label=f"{measurement['v_inf']}m/s")
     ax.set_xscale("log")
     ax.set_xlim(x_lim_0, x_lim_1)
