66264f7bd0add0746a8fba66394d228be0611235
prettykitty08 <t.p.libes@student.tudelft.nl>
2023-03-23 09:48:46 +0100

    implementation
    implementing funcs

--- BPM/main.py
+++ BPM/main.py
@@ -2,21 +2,32 @@ import common.py
 import math
 import numpy as np
 import math
+import TE_Bluntness.py
+import TipVortex.py
+import
 
 """
 Experimental Variables
 
 """
+#angle between sloping surfaces unstream
+psi = 14
+#bluntness 
+h = 0.0025
 #viscosity
 visc = 1.48 * 10**-5
 #density
 rho = 1.225
 #span
-Span = 1                    
+span = 1                    
 #distance from source to receiver        
 r_e = common.r_e(0, 0, 1.775, 1.34)   
 #Boundary layer thickness              
 delta = 
+#
+delta_p = 
+# 
+delta_s = 
 #Angle from source streamwise axis                           
 Theta_e = np.pi / 2  
 #Angle from source source lateral y axis                       
@@ -50,3 +61,25 @@ Initializing frequency bands
 """
 
 def plot_SPL()
+
+def Blunt(f):
+    delta_avg = TE_Bluntness.delta_avg(delta_p, delta_s)
+    mu = TE_Bluntness.mu(h, delta_avg)
+    m = TE_Bluntness.m(h, delta_avg)
+    eta0 = TE_Bluntness.eta0(m, mu)
+    k = TE_Bluntness.k(eta0, m)
+    St = TE_Bluntness.St(f, h, U)
+    G4 = TE_Bluntness.G4(h, delta_avg, psi)
+    G5 = TE_Bluntness.G5(eta, eta0, k, m)
+    St_peak = TE_Bluntness.St_peak(h, delta_avg, psi)
+    eta = TE_Bluntness.eta(St, St_peak)
+    return TE_Bluntness(h,M,span,Dh,r_e,G4,G5)
+
+def TipVortex(f):
+    l = TipVortex.l(c, alpha_tip)
+    M_max = TipVortex.M_max(M, alpha_tip)
+    st = TipVortex.st(f, l, U_max)
+    return TipVortex.SPL_tip(M, M_max, l, Dh, r_e, st)
+
+
+def LBLVS(f):
