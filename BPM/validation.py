6fc29e9cc95853eef1258c1aa6a85f6fef48753f
prettykitty08 <t.p.libes@student.tudelft.nl>
2023-04-25 12:30:46 +0200

    Its DONEEEE

--- BPM/validation.py
+++ BPM/validation.py
@@ -26,15 +26,15 @@ r_e = 1
 #Boundary layer thickness              
 delta = 0.1
 #displacement thickness
-delta_p = 0.001
+delta_p = 0.1
 #displacement thickness of suction side of airfoil
-#delta_s = 0.01
+delta_s = 0.001
 #Angle from source streamwise axis                           
 Theta_e = np.pi / 2  
 #Angle from source source lateral y axis                       
 Phi_e = np.pi / 2   
 #effective aerodynamic angle of attack                        
-alpha_star = 1.516 * np.pi / 180  
+alpha_star = 1.516 
 #temperature K                 
 T = 288.15    
 #chord m                           
@@ -46,7 +46,7 @@ U_max = 100
 #speed of sound  
 c_0 = 340.46
 #Free stream velocity    
-U = 100      
+U = 100  
 #flow mach number                        
 M = U / c_0    
 #convection mach number                                 
@@ -113,7 +113,7 @@ def TBL_TE(f):
     B_R = TBL.B_R1(B_min, B_max)
     B = TBL.B1(B_min, B_R, B_max)
     a0 = TBL.a01(Reynolds) # I started from here 
-    a = TBL.a1(St_p, St_peak) 
+    a = TBL.a1(St_s, St_peak) 
     A_min = TBL.A_min1(a)
     A_max = TBL.A_max1(a)
     A_R = TBL.A_R1(A_min, A_max)
@@ -121,61 +121,100 @@ def TBL_TE(f):
     SPL_alpha = TBL.SPL_alpha1(delta_s, M, L, Dh, r_e, B, St_s, St_2, K2)
     SPL_s = TBL.SPL_s1(delta_s, M, L, Dh, r_e, A, St_s, St_1, K1)
     SPL_p = TBL.SPL_p1(delta_p, M, L, Dh, r_e, A, St_p, St_1, K1, deltaK1)
-    return TBL.SPL_tot1(SPL_alpha, SPL_s, SPL_p)
+    SPL_tot =  TBL.SPL_tot1(SPL_alpha, SPL_s, SPL_p)
+    return SPL_s, SPL_p, SPL_alpha, SPL_tot
     #TBL.SPL_TBLTE1(SPL_s, SPL_p)
     
 
 def CalculateSPL(): 
     f = np.arange(1,10000)
-    SPLTip = []
-    SPLBlunt = []
-    SPLLBL = []
-    SPLTBL = []
+    SPLTBL_tot = []
+    SPLTBL_alpha = []
+    SPLTBL_p = []
+    SPLTBL_s = []
     for i in range(len(f)):
-        SPLTip.append(TipVortex(f[i]))
-        SPLBlunt.append(Blunt(f[i]))
-        SPLLBL.append(LBL(f[i]))
-        SPLTBL.append(TBL_TE(f[i]))
+        # SPLTip.append(TipVortex(f[i]))
+        # SPLBlunt.append(Blunt(f[i]))
+        # SPLLBL.append(LBL(f[i]))
+        SPLTBL_s.append(TBL_TE(f[i])[0])
+        SPLTBL_p.append(TBL_TE(f[i])[1])
+        SPLTBL_alpha.append(TBL_TE(f[i])[2])
+        SPLTBL_tot.append(TBL_TE(f[i])[3])
 
-    return f, SPLTip, SPLBlunt, SPLLBL, SPLTBL
+    return f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot
 
 # def plot():
-#     f, SPLTip, SPLBlunt, SPLLBL, SPLTBL = CalculateSPL()
-#     print(SPLTBL)
+#     f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL()
+#     print(SPLTBL_tot)
 #     fig, axs = plt.subplots(2, 2)
-#     axs[0, 0].plot(f, SPLTip)
-#     axs[0, 0].set_title('Tip Vortex')
+#     axs[0, 0].plot(f, SPLTBL_p)
+#     axs[0, 0].set_title('Pressure Side SPL')
 #     axs[0, 0].set_ylim((0, 100))
-#     axs[0, 1].plot(f, SPLBlunt, 'tab:orange')
-#     axs[0, 1].set_title('Bluntness')
+#     axs[0, 0].set_xscale('log')
+#     axs[0, 1].plot(f, SPLTBL_s, 'tab:orange')
+#     axs[0, 1].set_title('Suction Side SPL')
 #     axs[0, 1].set_ylim((0, 100))
-#     axs[1, 0].plot(f, SPLLBL, 'tab:green')
-#     axs[1, 0].set_title('LBL-VS')
+#     axs[0, 1].set_xscale('log')
+#     axs[1, 0].plot(f, SPLTBL_alpha, 'tab:green')
+#     axs[1, 0].set_title('SPL due to Alpha')
 #     axs[1, 0].set_ylim((0, 100))
-#     axs[1, 1].plot(f, f, 'tab:red')
-#     axs[1, 1].set_title('TBL-TE')
+#     axs[1, 0].set_xscale('log')
+#     axs[1, 1].plot(f, SPLTBL_tot, 'tab:red')
+#     axs[1, 1].set_title('Total SPL')
 #     axs[1, 1].set_ylim((0, 100))
+#     axs[1, 1].set_xscale('log')
+
 
 #     #print(SPLLBL)
     
 #     for ax in axs.flat:
 #         ax.set(xlabel='Frequency', ylabel='SPL')
-#     # for ax in axs.flat:
-#     #     ax.label_outer()
+#     for ax in axs.flat:
+#         ax.label_outer()
 #     plt.show()
 
 # plot()
 
 
-def plotnew():
-    f, SPLTip, SPLBlunt, SPLLBL, SPLTBL = CalculateSPL()
-    print(SPLTBL)
-    plt.plot(f, SPLTBL)
-    plt.ylim((0,100))
-    plt.xlabel("Frequency")
-    plt.ylabel("SPL")
-    plt.title("TBL-TE")
+# def plotnew():
+#     f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL()
+#     plt.plot(f, SPLTBL_tot)
+#     plt.ylim((0,100))
+#     plt.xlabel("Frequency")
+#     plt.ylabel("SPL")
+#     plt.title("TBL-TE")
+#     plt.show()
+
+
+# plotnew()
+
+def plotone():
+    f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL()
+    dotf = []
+    dots = []
+    dotp = []
+    dota = []
+    dottot = []
+    for i in range(1, len(f), 1000):
+        dotf.append(f[i])
+        dots.append(SPLTBL_s[i])
+        dotp.append(SPLTBL_p[i])
+        dota.append(SPLTBL_alpha[i])
+        dottot.append(SPLTBL_tot[i])
+
+    plt.scatter(dotf, dotp)
+    plt.scatter(dotf, dots)
+    plt.scatter(dotf, dota)
+    plt.scatter(dotf, dottot)
+    plt.plot(f, SPLTBL_p)
+    plt.plot(f, SPLTBL_s, 'tab:orange')
+    plt.plot(f, SPLTBL_alpha, 'tab:green')
+    plt.plot(f, SPLTBL_tot, 'tab:red')
+    plt.legend(["Pressure Side", "Suction Side", "Alpha", "Total SPL"])
+    plt.ylim((0, 100))
+    plt.xscale('log')
+    plt.title("SPL vs Frequency")
     plt.show()
 
 
-plotnew()
+plotone()
