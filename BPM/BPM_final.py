a5339c96009b5fa5e8725d4c94b7d289daccdb18
prettykitty08 <t.p.libes@student.tudelft.nl>
2023-05-09 13:26:32 +0200

    Updates from tutor meeting on BPM_final

--- BPM/BPM_final.py
+++ BPM/BPM_final.py
@@ -11,7 +11,7 @@ Theta_e = np.pi / 2
 Phi_e = np.pi / 2   
 c_0 = np.sqrt(1.4 * 287 * 288.15)
 rho = 1.225
-c = 1
+c = 0.15
 
 """
 These arrays contain the inputs per section we found
@@ -19,7 +19,7 @@ these using XFOIL for each section and calculated the velocity
 using the angular velocity of the turbine
 """
 
-U = np.array([5.6, 11.2, 16.8, 22.4, 28, 33.6, 39.2, 44.8, 50.4, 56])
+U = np.array([9.765, 13.764, 18.6, 23.78, 29.12, 34.54, 40, 45.51, 51.03, 56.57])
 delta_p = 0.1 * np.array([0.224, 0.194, 0.133, 0.0908, 0.11, 0.156, 0.134, 0.15, 0.18, 0.0295])
 delta_s = 0.1 * np.array([0.133, 0.383, 0.13, 0.106, 0.134, 0.1, 0.118, 0.131, 0.1105, 0.28])
 
@@ -29,12 +29,12 @@ delta_s = 0.1 * np.array([0.133, 0.383, 0.13, 0.106, 0.134, 0.1, 0.118, 0.131, 0
 
 M = U / c_0
 M_c = 0.8 * M                  
-#alpha_star = 1.516
-alpha_star = 0
+alpha_star = np.degrees(np.arctan(1/7))
 Reynolds = rho * U * c / visc
 L = 0.1
 r_e = 1.22
-Dh = common.Dh_bar(Theta_e, Phi_e, M, M_c)      
+#Dh = common.Dh_bar(Theta_e, Phi_e, M, M_c)  
+Dh = 1    
 R_deltaPstar = delta_p * U / visc
 
 """
@@ -43,7 +43,7 @@ BPM paper we checked multiple times and are quite confident the
 functions are correct
 
 """
-def TBL_TE(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar, Dh):
+def TBL_TE(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar):
     gamma = TBL.gamma1(M)
     gamma0 = TBL.gamma01(M)
     beta = TBL.beta1(M)
@@ -59,20 +59,27 @@ def TBL_TE(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar, Dh):
     St_peak = TBL.St_peak1(St_1, St_2, St_1mean)
     b0 = TBL.b01(Reynolds)
     b = TBL.b1(St_p, St_2)
-    B_min = TBL.B_min1(b0)
-    B_max = TBL.B_max1(b0)
-    B_R = TBL.B_R1(B_min, B_max)
+    B_min = TBL.B_min1(b)
+    B_max = TBL.B_max1(b)
+    B_min1 = TBL.B_min1(b0)
+    B_max1 = TBL.B_max1(b0)
+    B_R = TBL.B_R1(B_min1, B_max1)
     B = TBL.B1(B_min, B_R, B_max)
     a0 = TBL.a01(Reynolds) # I started from here 
     a = TBL.a1(St_s, St_peak) 
     A_min = TBL.A_min1(a)
     A_max = TBL.A_max1(a)
-    A_R = TBL.A_R1(A_min, A_max)
+    A_min1 = TBL.A_min1(a0)
+    A_max1 = TBL.A_max1(a0)
+    A_R = TBL.A_R1(A_min1, A_max1)
     A = TBL.A1(A_min, A_R, A_max) 
     SPL_alpha = TBL.SPL_alpha1(delta_s, M, L, Dh, r_e, B, St_s, St_2, K2)
     SPL_s = TBL.SPL_s1(delta_s, M, L, Dh, r_e, A, St_s, St_1, K1)
     SPL_p = TBL.SPL_p1(delta_p, M, L, Dh, r_e, A, St_p, St_1, K1, deltaK1)
     SPL_tot =  TBL.SPL_tot1(SPL_alpha, SPL_s, SPL_p)
+    if f % 500 == 0:
+        print(b0,b,St_s, St_2, B_R, B, B_max, B_min, f)
+
     return SPL_s, SPL_p, SPL_alpha, SPL_tot
 
 """
@@ -88,18 +95,19 @@ the rows correspond to 10 sections and the columns are frequencies
 
 """
 
-def CalculateSPL(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar, Dh): 
+def CalculateSPL(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar): 
     SPLTBL_tot = np.zeros((10, len(f)))
     SPLTBL_alpha = np.zeros((10, len(f)))
     SPLTBL_p = np.zeros((10, len(f)))
     SPLTBL_s = np.zeros((10, len(f)))
     for k in range(10):
         for i in range(len(f)):
-            SPLTBL_s[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k], Dh[k])[0]
-            SPLTBL_p[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k], Dh[k])[1]
-            SPLTBL_alpha[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k], Dh[k])[2]
-            SPLTBL_tot[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k], Dh[k])[3]
-
+            SPLTBL_s[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k])[0]
+            SPLTBL_p[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k])[1]
+            SPLTBL_alpha[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k])[2]
+            SPLTBL_tot[k, i] = TBL_TE(f[i], U[k], delta_s[k], delta_p[k], Reynolds[k], M[k], R_deltaPstar[k])[3]
+        print("Section", k)
+        print(Reynolds[k])
     return SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot
 
 """
@@ -121,7 +129,7 @@ def AddSections(a, b, c, d, f):
         SPL_p[i] = 10 * np.log10(sum(10**(b[:,i]/10)))
         SPL_alpha[i] = 10 * np.log10(sum(10**(c[:,i]/10)))
         SPL_tot[i] = 10 * np.log10(sum(10**(d[:,i]/10)))
-    
+
     return SPL_s, SPL_p, SPL_alpha, SPL_tot
 
 
@@ -154,7 +162,11 @@ def plotone(f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot):
     plt.title("SPL vs Frequency")
     plt.show()
 
-f = np.arange(1,10000)
-SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar, Dh)
+f = np.arange(200,10000)
+SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL(f, U, delta_s, delta_p, Reynolds, M, R_deltaPstar)
 SPL_s, SPL_p, SPL_alpha, SPL_tot = AddSections(SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot, f)
 plotone(f, SPL_s, SPL_p, SPL_alpha, SPL_tot)
+#np.set_printoptions(threshold=np.inf)
+#print(SPL_tot)
+
+
