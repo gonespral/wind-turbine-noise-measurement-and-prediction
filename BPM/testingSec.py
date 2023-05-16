dc2c90634d8447bc55c91223e05cd46065499853
prettykitty08 <t.p.libes@student.tudelft.nl>
2023-05-16 10:32:56 +0200

    new inputs

--- BPM/testingSec.py
+++ BPM/testingSec.py
@@ -3,27 +3,29 @@ import matplotlib.pyplot as plt
 import TBL_TE_Real as TBL
 import pickle as pkl
 
-c = 0.3048
+c = 0.15
+#c = 0.3048
 rho = 1.225
 visc = 1.4529 * 10**-5
 c_0 = 340.46
-L = 0.04572
+L = 0.1
+#L = 0.4572
 r_e = 1.22
 Dh = 1   
-alpha_star = 1.516
-U = 71.3
+alpha_star = np.rad2deg(np.arctan(2/21))
+#alpha_star = 1.516
+U = 56.253
 
-# delta_p = 0.00192024
-# delta_s = 0.00246888
+delta_p = 0.00057
+delta_s = 0.00465
 
 Reynolds = rho * U * c / visc
-delta_zero = c * 10**(3.411 - 1.5397*np.log10(Reynolds) + 0.1059*(np.log10(Reynolds))**2)
-delta_p = 10**(-0.0432*alpha_star + 0.00113*alpha_star**2) * delta_zero
-delta_s = 10**(0.0311*alpha_star) * delta_zero
+# delta_zero = c * 10**(3.411 - 1.5397*np.log10(Reynolds) + 0.1059*(np.log10(Reynolds))**2)
+# delta_p = 10**(-0.0432*alpha_star + 0.00113*alpha_star**2) * delta_zero
+# delta_s = 10**(0.0311*alpha_star) * delta_zero
 
 
 M = U / c_0
-#alpha_star = np.rad2deg(np.arctan(2/21))
 R_deltaPstar = delta_p * U / visc
 
 
@@ -68,7 +70,7 @@ def TBL_TE(f):
     #     print(St_1)
 
 
-    return SPL_p
+    return SPL_s, SPL_p, SPL_alpha, SPL_tot
 
 #import pickle
 
@@ -78,18 +80,18 @@ def CalculateSPL(f):
     SPLTBL_p = []
     SPLTBL_s = []
     for i in f:
-        #SPLTBL_s.append(TBL_TE(i)[0])
-        SPLTBL_p.append(TBL_TE(i))
-        #SPLTBL_alpha.append(TBL_TE(i)[2])
-        #SPLTBL_tot.append(TBL_TE(i)[3])
-    return SPLTBL_p
+        SPLTBL_s.append(TBL_TE(i)[0])
+        SPLTBL_p.append(TBL_TE(i)[1])
+        SPLTBL_alpha.append(TBL_TE(i)[2])
+        SPLTBL_tot.append(TBL_TE(i)[3])
+    return SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot
 
 
-def plotone(f, SPLTBL_p):
+def plotone(f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot):
     plt.plot(f, SPLTBL_p)
-    #plt.plot(f, SPLTBL_s, 'tab:orange')
-    #plt.plot(f, SPLTBL_alpha, 'tab:green')
-    #plt.plot(f, SPLTBL_tot, 'tab:red')
+    plt.plot(f, SPLTBL_s, 'tab:orange')
+    plt.plot(f, SPLTBL_alpha, 'tab:green')
+    plt.plot(f, SPLTBL_tot, 'tab:red')
     plt.legend(["Pressure Side", "Suction Side", "Alpha", "Total SPL"])
     plt.ylim((20, 70))
     plt.xscale('log')
@@ -107,8 +109,8 @@ def plotone(f, SPLTBL_p):
 
 print(TBL_TE(100))
 f = np.arange(1,10000)
-SPLTBL_p = CalculateSPL(f)
-#plotone(f, SPLTBL_p)
+SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot = CalculateSPL(f)
+plotone(f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot)
 
 # import pickle
 # import pickle
@@ -116,3 +118,5 @@ SPLTBL_p = CalculateSPL(f)
 # Save data to pickle file
 #with open('saves/BPM_spl.pkl', 'wb') as f_:
 #    pkl.dump([f, SPLTBL_s, SPLTBL_p, SPLTBL_alpha, SPLTBL_tot], f_)
+
+#import pickle
